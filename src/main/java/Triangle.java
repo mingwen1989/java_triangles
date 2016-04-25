35cd88f3acb6c14dd073bdd96ab7ed7aae2095a2
Darius Capizzi and Ming Wen <darius@>
2016-04-25 09:49:51 -0700

    Add triangle tests

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,28 +1,32 @@
 public class Triangle {
 
-    private int side1;
-    private int side2;
-    private int side3;
+  private int side1;
+  private int side2;
+  private int side3;
 
-  public Triangle(int side1, int side2, int side3) {
-    side1 = side1;
-    side2 = side2;
-    side3 = side3;
+  public Triangle(int inSide1, int inSide2, int inSide3) {
+    side1 = inSide1;
+    side2 = inSide2;
+    side3 = inSide3;
   }
 
   public String testSide(){
+    System.out.println(side1);
+    if(side1 + side2 < side3 || side2 + side3 < side1 || side3 + side1 < side2){ //if any two sides are less than the third)
+      return "Not a triangle";
+    }
+
+
     if (side1 == side2 && side2 == side3){
       return "Equilateral";
     }
     else if (side1 == side2 || side2 == side3 || side3 == side1) {
       return "Isosceles";
     }
-    else if (side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2){ //if any two sides are greater than the third
+    else {
       return "Scalene";
     }
-    else {
-      return "Not a triangle";
-    }
+
   }
 
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -3,9 +3,33 @@ import static org.junit.Assert.*;
 
 public class TriangleTest {
 
+  // @Test
+  // public void newTriangle_instantiatesCorrectly_true() {
+  //   Triangle testTriangle = new Triangle(4, 4, 4);
+  //   assertEquals(true, testTriangle instanceof Triangle);
+  // }
+
   @Test
-  public void newTriangle_instantiatesCorrectly() {
+  public void newTriangle_testSidemethod_Equalateral() {
     Triangle testTriangle = new Triangle(4, 4, 4);
-    assertEquals(true, testTriangle instanceof Triangle);
+    assertEquals("Equilateral", testTriangle.testSide());
+  }
+
+  @Test
+  public void newTriangle_testSidemethod_Isosceles() {
+    Triangle testTriangle = new Triangle(4, 4, 1);
+    assertEquals("Isosceles", testTriangle.testSide());
+  }
+
+  @Test
+  public void newTriangle_testSidemethod_Scalene() {
+    Triangle testTriangle = new Triangle(4, 3, 2);
+    assertEquals("Scalene", testTriangle.testSide());
+  }
+
+  @Test
+  public void newTriangle_testSidemethod_NotTriangle() {
+    Triangle testTriangle = new Triangle(4, 3, 100);
+    assertEquals("Not a triangle", testTriangle.testSide());
   }
 }
