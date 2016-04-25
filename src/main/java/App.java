04e5ba41d52d40ed0d19a1dacd49f25bedaf94c3
Darius Capizzi and Ming Wen <ming@>
2016-04-25 10:20:39 -0700

    Added in if statement in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,7 +16,27 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
 
+      Integer side1input = Integer.parseInt(request.queryParams("side1input"));
+      Integer side2input = Integer.parseInt(request.queryParams("side2input"));
+      Integer side3input = Integer.parseInt(request.queryParams("side3input"));
+
+      Triangle testTriangle = new Triangle(side1input, side2input, side3input);
+      String result = testTriangle.testSide();
+
+      model.put("side1input", side1input);
+      model.put("side2input", side2input);
+      model.put("side3input", side3input);
+      if (result == "Isosceles") {
+        model.put("result", "ahhhhh blaahh isosceles");
+      } else {
+        model.put("result", result);
+      }
+      model.put("template","templates/results.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
 }
